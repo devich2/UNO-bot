becb2adc1bebeba9aafc40b82e3d4d9e2c62a0c5
devich2 <38702365+devich2@users.noreply.github.com>
2019-12-24 05:50:04 +0200

    Update websocket-server.js

--- websocket-server.js
+++ websocket-server.js
@@ -125,7 +125,7 @@ async function get_cards(data) {
     const possible = game.possible_cards;
     cards = cards.map((val) => {
       let available = possible.findIndex((v) => v.light == val.id)
-      console.light(val.id, find_card(val.id))
+      console.log(val.id, find_card(val.id))
       return available >= 0 ? {id: val.id, valid: true} : {id: find_card(val.id).dark, valid: false};
     })
     card_result = cards == [] ? {
